7cccec809e02d6abe180ee531dcc69567858805e
dpwang <dongpeng.wang@anlogic.com>
2020-04-12 20:20:21 +0800

    Compiler in Go: finish ch10

--- go/src/monkey/compiler/compiler.go
+++ go/src/monkey/compiler/compiler.go
@@ -86,12 +86,12 @@ func (c *Compiler) Compile(node ast.Node) error {
             c.emit(code.OpPop)
 
         case *ast.LetStatement:
+            symbol := c.symbolTable.Define(node.Name.Value)
             err := c.Compile(node.Value)
             if err != nil {
                 return err
             }
 
-            symbol := c.symbolTable.Define(node.Name.Value)
             if symbol.Scope == GlobalScope {
                 c.emit(code.OpSetGlobal, symbol.Index)
             } else {
